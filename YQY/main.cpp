de5892eaa7651006577f8d2ecea9fe7a7c4d58d8
yangqiuyou <wangyi168359476@163.com>
2026-03-18 17:51:59 +0800

    补充提交main文件

--- YQY/main.cpp
+++ YQY/main.cpp
@@ -1,36 +1,39 @@
-#include "GUI/YQY.h"
+﻿#include "GUI/YQY.h"
 #include <QtWidgets/QApplication>
 #include "Import/Input_Model.h"
 #include "DataStructure/Structure/StructureData.h"
 #include "Solver/Solver.h"
 #include "Conductor/ConductorLib.h"
+#include <Windows.h>
+
+#include "Import/AeroManager.h"
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
-    auto pStructure = std::make_shared<StructureData>();
+    //auto pStructure = std::make_shared<StructureData>();
 
-    Input_Model importer;
+    //Input_Model importer;
 
-    QString BaseName = "斜杆一端受压";
-    QString InputPath = QString("Import/ImportFile/%1.bdf").arg(BaseName);
-    QString OutputPath = QString("Export/ExportFile/%1_TEP.bdf").arg(BaseName);
+    //QString BaseName = "斜杆一端受压";
+    //QString InputPath = QString("Import/ImportFile/%1.bdf").arg(BaseName);
+    //QString OutputPath = QString("Export/ExportFile/%1_TEP.bdf").arg(BaseName);
 
-    qDebug().noquote() << QStringLiteral("\n读取文件为:") << InputPath << "\n";
-    if (importer.InputData(InputPath, pStructure))
-    {
-        qDebug() << "\n=====Model loaded successfully!=====";
+    //qDebug().noquote() << QStringLiteral("\n读取文件为:") << InputPath << "\n";
+    //if (importer.InputData(InputPath, pStructure))
+    //{
+    //    qDebug() << "\n=====Model loaded successfully!=====";
 
-    //     使用 Solver 运行分析
-        Solver solver;
-        solver.SetStructure(pStructure);
-        solver.RunAll();  // 运行所有分析步
+    ////     使用 Solver 运行分析
+    //    Solver solver;
+    //    solver.SetStructure(pStructure);
+    //    solver.RunAll();  // 运行所有分析步
 
-        std::vector<int> nodeIds = {  2 };
-        std::vector<DataType> types = { DataType::U1, DataType::U2, DataType::U3, DataType::F1, DataType::F2, DataType::F3 };
+    //    std::vector<int> nodeIds = {  2 };
+    //    std::vector<DataType> types = { DataType::U1, DataType::U2, DataType::U3, DataType::F1, DataType::F2, DataType::F3 };
 
-        pStructure->GetOutputter().ExportNodes(OutputPath, nodeIds, types);
-    }
+    //    pStructure->GetOutputter().ExportNodes(OutputPath, nodeIds, types);
+    //}
 
     //ConductorLib::Config cfg;
     //cfg.nBundle = 4; // 四分裂
@@ -83,6 +86,49 @@ int main(int argc, char* argv[])
     //pStructure->GetOutputter().ExportNodes(OutputPath, nodeIds, types);
 
     //pStructure->GetOutputter().SaveModel("Export/ExportFile/123456.bdf", pStructure.get());
+
+
+
+
+    // 启用控制台颜色支持
+    enableConsoleColor();
+
+    // 设置控制台输出编码为UTF-8
+    SetConsoleOutputCP(CP_UTF8);
+
+    AeroManager manager;
+
+    std::string Filename = "1-14ms-12mm.csv";
+    //读取CSV文件，然后输出读取的数据到另一个CSV文件
+    bool success = manager.loadCSV("Import/Aero_Data/Input_Data/" + Filename);
+
+    if (!success)
+    {
+        std::cerr << "\n 无法加载测试数据，程序终止。\n";
+        return -1;
+    }
+    //
+    //manager.exportToCSV("C_Data/Output_Data/" + Filename, 6);
+
+    //for (double i = 1.5; i <= 7; i += 5)
+    //{
+    //    std::cout << "\nLift = " << manager.getData(0, CoefType::LIFT, i); // 测试插值
+    //    std::cout << "\nDrag = " << manager.getData(0, CoefType::DRAG, i); // 测试插值
+    //    std::cout << "\nMoment = " << manager.getData(0, CoefType::MOMENT, i); // 测试插值
+
+    //}
+
+
+    // 自动扫描Output_Data文件夹，与C_Data中的同名文件对比 
+    //验证气动参数读取是否有问题
+
+    bool allPassed = manager.ValiDateAllCSV(
+        "Import/Aero_Data/Output_Data",   // 输出文件夹
+        "Import/Aero_Data/Input_Data",    // 标准文件夹
+        0.000001                // 误差容限
+    );
+
+
     YQY window;
     //window.show();
     return app.exec();
